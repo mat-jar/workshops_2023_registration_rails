0b1f9a37504d5bc9b5a43c971b75e3b4b039bac1
mat-jar <mtu07@poczta.fm>
2023-05-02 00:19:41 +0200

    Change type of column 'deadline' in tasks table to datetime+update seeds

---
+++ db/migrate/20230501220828_change_deadline_type_in_tasks.rb
@@ -0,0 +1,5 @@
+class ChangeDeadlineTypeInTasks < ActiveRecord::Migration[7.0]
+  def change
+    change_column(:tasks, :deadline, :datetime)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Task.create(
   [
-    { title: 'Water the plants', description: 'Give your indoor or outdoor plants the water they need to thrive.', deadline: 1683321879 },
-    { title: 'Do the dishes', description: 'Clean and put away any dirty dishes in the sink or on the counter.', deadline: 1683408279 },
-    { title: 'Take out the trash', description: 'Collect and dispose of any garbage in your home.', deadline: 1683494679 },
-    { title: 'Vacuum the carpet', description: 'Use a vacuum cleaner to remove dirt and debris from the carpeted areas of your home.', deadline: 1683581079 },
-    { title: 'Fold the laundry', description: 'Sort and fold any clean laundry you have to keep it organized and easy to find.', deadline: 1683667479 },
-    { title: 'Clean the bathroom', description: 'Wipe down surfaces, scrub the toilet and bathtub, and clean the mirror in your bathroom.', deadline: 1683753879 },
-    { title: 'Make the bed', description: 'Straighten up your bedding and arrange your pillows for a clean and inviting look.', deadline: 1683840279 },
-    { title: 'Sweep the floor', description: 'Use a broom to remove dirt and debris from hard flooring surfaces in your home.', deadline: 1683926679 },
-    { title: 'Take a walk', description: 'Get some fresh air and exercise by taking a leisurely walk around your neighborhood or local park.', deadline: 1684013079 },
-    { title: 'Read a book', description: 'Take some time to relax and enjoy a good book, whether its a classic novel or a new release.', deadline: 1684099479 }
+    { title: 'Water the plants', description: 'Give your indoor or outdoor plants the water they need to thrive.', deadline: DateTime.new(2023,5,7,17,45)},
+    { title: 'Do the dishes', description: 'Clean and put away any dirty dishes in the sink or on the counter.', deadline: DateTime.new(2023,5,8,17,45)},
+    { title: 'Take out the trash', description: 'Collect and dispose of any garbage in your home.', deadline: DateTime.new(2023,5,8,17,45) },
+    { title: 'Vacuum the carpet', description: 'Use a vacuum cleaner to remove dirt and debris from the carpeted areas of your home.', deadline: DateTime.new(2023,5,10,17,45) },
+    { title: 'Fold the laundry', description: 'Sort and fold any clean laundry you have to keep it organized and easy to find.', deadline: DateTime.new(2023,5,11,17,45) },
+    { title: 'Clean the bathroom', description: 'Wipe down surfaces, scrub the toilet and bathtub, and clean the mirror in your bathroom.', deadline: DateTime.new(2023,5,12,17,45) },
+    { title: 'Make the bed', description: 'Straighten up your bedding and arrange your pillows for a clean and inviting look.', deadline: DateTime.new(2023,5,13,17,45) },
+    { title: 'Sweep the floor', description: 'Use a broom to remove dirt and debris from hard flooring surfaces in your home.', deadline: DateTime.new(2023,5,14,17,45) },
+    { title: 'Take a walk', description: 'Get some fresh air and exercise by taking a leisurely walk around your neighborhood or local park.', deadline: DateTime.new(2023,5,15,17,45) },
+    { title: 'Read a book', description: 'Take some time to relax and enjoy a good book, whether its a classic novel or a new release.', deadline: DateTime.new(2023,5,16,17,45) }
   ]
 )
